cb0a24d2924b296fd2b88ed002701c06bb1eb416
Fabrice Pinglot <fabricepinglot@gmail.com>
2019-06-06 19:15:43 +0200

    add form component

--- moisson-live/src/app/app.module.ts
+++ moisson-live/src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+//import { ChartsModule } from 'ng2-charts';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 import { FooterComponent } from './footer/footer.component';
 import { MapComponent } from './map/map.component';
+//import { ComparatorComponent } from './comparator/comparator.component';
+
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-
-    FooterComponent
-    MapComponent
+    FooterComponent,
+    MapComponent,
+    //ComparatorComponent,
+   // ChartsModule,
+    FormComponent
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
