d9e3d33a9c8406828b86bd7934fc36aeabe866da
Lyneeee <namiss.lyne@gmail.com>
2019-06-06 19:18:56 +0200

    add header

--- moisson-live/src/app/app.module.ts
+++ moisson-live/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { MapComponent } from './map/map.component';
 import { HeaderComponent } from './header/header.component';
+import { PictureComponent } from './picture/picture.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { HeaderComponent } from './header/header.component';
 
     FooterComponent,
     MapComponent,
-    HeaderComponent
+    HeaderComponent,
+    PictureComponent
 
   ],
   imports: [
